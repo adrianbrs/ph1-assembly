59658a30f71b07c853b0776617c5a46f1f13ade2
Luiz Gallas <luizgallas@outlook.com>
2020-06-09 17:14:14 -0300

    Adicionado pacote constants, alterada segunda passage, corrigido bug na primeira passagem, subtituidos textos fixos pelas constantes

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -2,6 +2,7 @@ package decoder
 
 import (
 	"fmt"
+	"ph1-assembly/constants"
 )
 
 type metaInstruction struct {
@@ -11,22 +12,22 @@ type metaInstruction struct {
 
 // Mapeia todos os mnemônicos disponíveis para seus opCodes e número de endereços
 var operations = map[string]*metaInstruction{
-	"NOP": &metaInstruction{opCode: "00", size: 0},
-	"LDR": &metaInstruction{opCode: "01", size: 1},
-	"STR": &metaInstruction{opCode: "02", size: 1},
-	"ADD": &metaInstruction{opCode: "03", size: 1},
-	"SUB": &metaInstruction{opCode: "04", size: 1},
-	"MUL": &metaInstruction{opCode: "05", size: 1},
-	"DIV": &metaInstruction{opCode: "06", size: 1},
-	"NOT": &metaInstruction{opCode: "07", size: 1},
-	"AND": &metaInstruction{opCode: "08", size: 0},
-	"OR":  &metaInstruction{opCode: "09", size: 1},
-	"XOR": &metaInstruction{opCode: "A0", size: 1},
-	"JMP": &metaInstruction{opCode: "B0", size: 1},
-	"JEQ": &metaInstruction{opCode: "C0", size: 1},
-	"JG":  &metaInstruction{opCode: "D0", size: 1},
-	"JL":  &metaInstruction{opCode: "E0", size: 1},
-	"HLT": &metaInstruction{opCode: "F0", size: 0},
+	"NOP": &metaInstruction{opCode: "00", size: 1},
+	"LDR": &metaInstruction{opCode: "10", size: 2},
+	"STR": &metaInstruction{opCode: "20", size: 2},
+	"ADD": &metaInstruction{opCode: "30", size: 2},
+	"SUB": &metaInstruction{opCode: "40", size: 2},
+	"MUL": &metaInstruction{opCode: "50", size: 2},
+	"DIV": &metaInstruction{opCode: "60", size: 2},
+	"NOT": &metaInstruction{opCode: "70", size: 2},
+	"AND": &metaInstruction{opCode: "80", size: 1},
+	"OR":  &metaInstruction{opCode: "90", size: 2},
+	"XOR": &metaInstruction{opCode: "A0", size: 2},
+	"JMP": &metaInstruction{opCode: "B0", size: 2},
+	"JEQ": &metaInstruction{opCode: "C0", size: 2},
+	"JG":  &metaInstruction{opCode: "D0", size: 2},
+	"JL":  &metaInstruction{opCode: "E0", size: 2},
+	"HLT": &metaInstruction{opCode: "F0", size: 1},
 }
 
 // Decode traduz o mnemônico de uma instrução e retorna
@@ -35,7 +36,7 @@ func Decode(name string) (string, int, error) {
 	instruction := operations[name]
 
 	if instruction == nil {
-		return "", 0, fmt.Errorf("None instruction found for this name: %s", name)
+		return "", 0, fmt.Errorf(constants.NoneInstructionFound, name)
 	}
 
 	return instruction.opCode, instruction.size, nil
