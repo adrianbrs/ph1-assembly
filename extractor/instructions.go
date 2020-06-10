42a4ebde0a2fac430e8c7676fda413370fdefd8e
Luiz Gallas <luizgallas@outlook.com>
2020-06-10 16:24:36 -0300

    Ajustada segunda passagem

--- extractor/instructions.go
+++ extractor/instructions.go
@@ -16,17 +16,29 @@ type Instruction struct {
 	Data       *Data
 }
 
-//Data representa o valor que a operação carrega
+// Data representa os valores contidos na section data ou representa os valores
+// contidos no segundo byte da instrução, dependendo do contexto
 type Data struct {
 	Address int
 	Value   int
 }
 
-//ExtractInstructions efetua a segunda passagem no código, guardando as
-// instrucoes em uma lista de struct
-func ExtractInstructions(textContent []*input.SourceLine, labelMap map[string]int) []Instruction {
+//ExtractInstructionsAndData efetua a segunda passagem no código, guardando as
+// instrucoes em uma lista de struct e os dados da section Data na struct Data
+func ExtractInstructionsAndData(content *input.Source, labelMap map[string]int) (*[]Instruction, *[]Data) {
+	instructions := extractInstructions(content, labelMap)
+	datas := extractData(content.Data)
+
+	return instructions, datas
+}
+
+// Extrai as instruções e cria uma lista de instructions contendo endereço, opCode e os dados do segundo
+// byte(caso tenha)
+func extractInstructions(content *input.Source, labelMap map[string]int) *[]Instruction {
 	var instructions = make([]Instruction, 0)
 
+	dataContent := content.Data
+	textContent := content.Text
 	for _, srcLine := range textContent {
 
 		if srcLine.Name == constants.TextSection || srcLine.Name == constants.DataSection {
@@ -36,35 +48,60 @@ func ExtractInstructions(textContent []*input.SourceLine, labelMap map[string]in
 		opCode, size := decoder.DecodeText(srcLine.Name)
 
 		// Cria instrução sem operando
-		instruction := &Instruction{
+		instruction := Instruction{
 			Address: srcLine.Address,
 			OpCode:  opCode,
 		}
 
 		// Verifica se o valor retornado da decodificação para aquela instrução é 1 ou 2
 		if size == 2 {
-			instruction.HasOperand = true
-			// Busca nos labels o valor do operando
-			operandValue, found := labelMap[srcLine.Operand]
-
-			if found == false {
-				var err error
-				operandValue, err = strconv.Atoi(srcLine.Operand)
-
-				if err != nil {
-					panic(pherror.Format(pherror.LabelNotFound, srcLine.Operand))
-				}
-			}
-
-			instruction.Data.Value = operandValue
-
-			instruction.Data.Address = srcLine.Address + 1
+			extractDataLabel(dataContent, instruction, labelMap, srcLine)
 		}
 
 		// Adiciona a instrução na lista e executa o laço novamente
-		instructions = append(instructions, *instruction)
+		instructions = append(instructions, instruction)
 
 	}
 
-	return instructions
+	return &instructions
+}
+
+// Procura no label map o valor do operando
+func extractDataLabel(dataContent []*input.SourceLine, instruction Instruction, labelMap map[string]int,
+	instructionInfo *input.SourceLine) {
+	instruction.HasOperand = true
+
+	// Busca nos labels o valor do operando
+	operandValue, found := labelMap[instructionInfo.Operand]
+
+	// Caso não encontre
+	if found == false {
+		var err error
+		operandValue, err = strconv.Atoi(instructionInfo.Operand)
+
+		if err != nil {
+			panic(pherror.Format(pherror.LabelNotFound, instructionInfo.Operand))
+		}
+	}
+
+	data := &Data{
+		Value:   operandValue,
+		Address: instructionInfo.Address + 1,
+	}
+	instruction.Data = data
+}
+
+func extractData(content []*input.SourceLine) *[]Data {
+	var datas = make([]Data, 0)
+
+	for _, data := range content {
+		newData := Data{
+			Value:   data.Address,
+			Address: data.LineNumber,
+		}
+
+		datas = append(datas, newData)
+	}
+
+	return &datas
 }
